a5b268c9a20435cd3d800ec9b958491018332b1e
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-02-01 15:40:09 -0500

    Writing a class

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -11,6 +11,37 @@ export interface Directors extends Teacher {
     numberOfReports: number;
 }
 
-export function printTeacher(firstName: string, lastName: string) {
+export interface printTeacher {
+    (firstName: string, lastName: string): string;
+}
+let print: printTeacher;
+print = function (firstName: string, lastName: string): string {
     return `${firstName[0]}. ${lastName}`
+}
+
+export interface StudentConstructor {
+    new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+export interface StudentClassInterface {
+    displayName(): string;
+    workOnHomework(): string;
+}
+
+export class StudentClass {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework() {
+        return 'Currently working';
+    }
+
+    displayName() {
+        return this.firstName;
+    }
 }
