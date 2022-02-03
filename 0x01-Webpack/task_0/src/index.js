5e687b209d132126c01f5d131c2c25b4a75a789d
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-02-02 22:45:56 -0500

    Basic setup

---
+++ 0x01-Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from "jquery";
+
+let element = document.createElement('div');
+document.body.appendChild(element);
+$('div').append('<p>Holberton Dashboard</p>');
+$('div').append('<p>Dashboard data for the students</p>');
+$('div').append('<p>Copyright - Holberton School</p>');
