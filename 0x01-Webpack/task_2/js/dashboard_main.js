a7a0e144ba2afcbadad88ab3e0146052bc451932
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-02-03 21:54:50 -0500

    update file *.css and *.js

--- 0x01-Webpack/task_2/js/dashboard_main.js
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -3,6 +3,7 @@ import _ from 'lodash';
 import img from '../assets/holberton-logo.jpg';
 import "../css/main.css";
 
+$('body').append("<div id=logo></div>");
 $('body').append("<p>Holberton Dashboard</p>");
 $('body').append("<p>Dashboard data for the students</p>");
 $('body').append("<button>Click here to get started</button");
