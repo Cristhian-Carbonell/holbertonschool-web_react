a11539f48005033874a5371da69b70bcf6c32a2b
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-02-03 21:15:24 -0500

    update file webpack.config.js

--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -1,13 +1,7 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './js/dashboard_main.js',
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Output Management',
-    })
-  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
