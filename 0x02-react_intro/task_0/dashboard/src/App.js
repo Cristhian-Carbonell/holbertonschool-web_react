ca0e09d0bf1df4d9f120c7ea59c6bfc3f27d5be8
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-02-07 15:13:43 -0500

    Basic application

---
+++ 0x02-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,19 @@
+export default function App() {
+    return (
+        <div>
+            <div className="App-header">
+                <img src="https://github.com/Cristhian-Carbonell/holbertonschool-web_react/blob/main/0x02-react_intro/task_0/dashboard/src/logo.jpg?raw=true" alt="logo"></img>
+                <h1>School dashboard</h1>
+            </div>
+            <div className="linea"></div>
+            <div className="App-body">
+                <p>Login to access the full dashboard</p>
+            </div>
+            <div className="linea"></div>
+            <div className="App-footer">
+                <p>Copyright 2020 - holberton School</p>
+            </div>
+        </div>
+    );
+}
+  
