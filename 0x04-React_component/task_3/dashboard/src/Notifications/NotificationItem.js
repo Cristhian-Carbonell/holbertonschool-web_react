2896842c449fdb6431987d9a0fa02b04518283f4
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-02-24 10:12:49 -0500

    Create WithLogging HOC

---
+++ 0x04-React_component/task_3/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class NotificationItem extends React.Component {
+    render() {
+        if (this.props.value) return (<li data-notification-type={this.props.type} onClick={() => { this.props.markAsRead(this.props.id) }}>{this.props.value}</li>);
+        else return (<li data-notification-type={this.props.type} dangerouslySetInnerHTML={this.props.html} onClick={() => { this.props.markAsRead(this.props.id) }}></li>);
+    }
+}
+
+NotificationItem.propTypes = {
+    id: PropTypes.number.isRequired,
+    type: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    html: PropTypes.shape({'__html': PropTypes.string}),
+    markAsRead: PropTypes.func
+};
+
+NotificationItem.defaultProps = {
+    type: "default",
+    value: '',
+    html: {},
+    markAsRead: () => void(0)
+};
