1543e199e1276362c3d7152e0fa581bc094ddcbe
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-03-14 15:37:13 -0500

    List and push

---
+++ 0x07-React_Immutable/3-list.js
@@ -0,0 +1,12 @@
+import { List } from 'immutable';
+
+export function getListObject(array) {
+  return List(array);
+}
+
+export function addElementToList(list, element) {
+  return list.push(element);
+}
+
+const list = getListObject(['Apple', 'Banana']);
+addElementToList(list, 'Orange');
