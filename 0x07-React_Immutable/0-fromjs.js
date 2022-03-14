1d4c6d96fe36f4205b7e5a5310c3f5f7e73349e6
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-03-14 12:22:58 -0500

    converting into an immutable object using fromJS

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -1,12 +1,12 @@
 import { fromJS } from 'immutable';
 
 export default function getImmutableObject(object) {
-    return fromJS(object);
+  return fromJS(object);
 }
 
 const myList = getImmutableObject({
-    fear: true,
-    smell: -1033575916.9145899,
-    wall: false,
-    thing: -914767132
-})
+  fear: true,
+  smell: -1033575916.9145899,
+  wall: false,
+  thing: -914767132,
+});
