b9a30a5909151a499b8a453eaa12b48b0a71af61
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-03-14 17:22:54 -0500

    Merge & concat

--- 0x07-React_Immutable/5-merge.js
+++ 0x07-React_Immutable/5-merge.js
@@ -5,18 +5,18 @@ export function concatElements(page1, page2) {
 }
 
 export function mergeElements(page1, page2) {
-  return Map({...page1, page2});
+  return Map( {...page1, page2} );
 }
 
 const element = concatElements(
   ['Apple', 'Banana'],
   ['Alex', 'Carbonell'],
-)
+);
 
 const element1 = mergeElements(
-  {'Apple': 'Banana'},
-  {'Alex': 'Carbonell'},
-)
+  { Apple: 'Banana' },
+  { Alex: 'Carbonell' },
+);
 
 console.log(element);
 console.log(element1);
