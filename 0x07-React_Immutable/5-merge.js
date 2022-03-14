2e5345d57b84c3421d93b11464e71292b9d8a9ad
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-03-14 17:25:53 -0500

    Merge & concat

--- 0x07-React_Immutable/5-merge.js
+++ 0x07-React_Immutable/5-merge.js
@@ -1,11 +1,11 @@
 import { List, Map } from 'immutable';
 
 export function concatElements(page1, page2) {
-  return List(page1.concat(page2));
+  return List(page1).concat(page2);
 }
 
 export function mergeElements(page1, page2) {
-  return Map( {...page1, page2} );
+  return Map(page1).merge(page2);
 }
 
 const element = concatElements(
