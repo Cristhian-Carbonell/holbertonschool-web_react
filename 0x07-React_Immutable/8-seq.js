77e896e7c60beb043041d5a02588e5d06930cafd
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-03-14 18:43:42 -0500

    Lazy Seq

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -1,16 +1,27 @@
 import { Seq } from 'immutable';
 
 export default function printBestStudents(object) {
-  let obj;
-
-  Seq(object)
-    .filter(value => value.score > 70)
-    .map((value) => obj = value).toObject();
-
-  const firstName = obj['firstName'].toUpperCase();
-  const lastName = obj['lastName'].toUpperCase();
-
-  console.log(firstName, lastName);
+    const seq = Seq(object);
+  
+    //   console.log(seq);
+    const filtered = seq.filter((student) => {
+      student.firstName.charAt(0).toUpperCase();
+      return student.score > 70;
+    });
+  
+    function capFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+  
+    const JSObject = filtered.toJS();
+  
+    Object.keys(JSObject).map((key) => {
+      JSObject[key].firstName = capFirstLetter(JSObject[key].firstName);
+      JSObject[key].lastName = capFirstLetter(JSObject[key].lastName);
+      return JSObject[key];
+    });
+  
+    console.log(JSObject);
 }
 
 const grades = {
