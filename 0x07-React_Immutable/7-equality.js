41c03f72c79e727ded8b4ddb0ff6dbacbaf9729b
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-03-14 17:56:01 -0500

    Equality

--- 0x07-React_Immutable/7-equality.js
+++ 0x07-React_Immutable/7-equality.js
@@ -1,20 +1,22 @@
-import { Map } from 'immutable';
+import { Map, is } from 'immutable';
 
 export default function areMapsEqual(map1, map2) {
-  return Map(map1).equals(map2);
+  const Object1 = Map(map1);
+  const Object2 = Map(map2);
+  return is(Object1, Object2);
 }
 
 const map1 = new Map(
   {
     firstName: 'Guillaume',
     lastName: 'Salva',
-  }
+  },
 );
 const map2 = new Map(
   {
     firstName: 'Guillaume',
     lastName: 'Salva',
-  }
+  },
 );
 
 console.log(areMapsEqual(map1, map2));
